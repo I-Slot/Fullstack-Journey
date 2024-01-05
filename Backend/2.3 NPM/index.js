b65b7a8a4bd6cdd4de90cee2a54659cc9846eb2a
github_i-slot <ismailadesholaharun@gmail.com>
2024-01-05 01:47:54 +0100

    QR code Project

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// let generateName = require("sillyname");
+
+// import generateName from "sillyname";
+// let sillyname = generateName();
+
+// console.log(`Example of sillyname is ${sillyname}`);
+
+import superhero from "superhero";
+let name = superhero.random();
+
+console.log(`I'm a ${name}`);
